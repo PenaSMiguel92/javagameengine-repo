aeb1f7baf0d5da8f9a05d8173644006c427b286b
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-09-12 23:17:47 -0700

    clean up

--- src/main/java/javagameengine/Engine/LevelEditorScene.java
+++ src/main/java/javagameengine/Engine/LevelEditorScene.java
@@ -9,30 +9,6 @@ import static org.lwjgl.opengl.GL30.*;
 
 public class LevelEditorScene extends Scene {
 
-    private String vertexShaderSrc = "#version 330 core\n" + //
-            "\n" + //
-            "layout (location=0) in vec3 aPos;\n" + //
-            "layout (location=1) in vec4 aColor;\n" + //
-            "\n" + //
-            "\n" + //
-            "out vec4 fColor;\n" + //
-            "\n" + //
-            "void main() {\n" + //
-            "    fColor = aColor;\n" + //
-            "    gl_Position = vec4(aPos, 1.0);\n" + //
-            "}";
-    private String fragmentShaderSrc = "#version 330 core\n" + //
-            "\n" + //
-            "in vec4 fColor;\n" + //
-            "\n" + //
-            "out vec4 color;\n" + //
-            "\n" + //
-            "void main() {\n" + //
-            "    color = fColor;\n" + //
-            "}";
-
-    private int vertexID, fragmentID, shaderProgram;
-
     private float[] vertexArray = {
         //position              //color
          0.5f, -0.5f,  0.0f,      1.0f, 0.0f, 0.0f, 1.0f, //bottom right    0
